5fcce9e4e2bca137ccc01a1fbad6f87fb353527b
timonsk <regetadm@gmail.com>
2015-03-24 01:54:57 +0200

    Implement simple Node JS module;

---
+++ SimpleModuleNodeJS/User/index.js
@@ -0,0 +1,11 @@
+﻿var db = require('Database');
+var log = require('logger')(module);
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function(who) {
+    log(db.GetPhrase('hello') + ", " + who.name);
+};
+module.exports = User;
---
+++ SimpleModuleNodeJS/app.js
@@ -0,0 +1,17 @@
+﻿var db = require('Database');
+var log = require('logger')(module);
+
+db.Connect();
+var User = require('./User');
+
+function Run() {
+    var timon = new User("Дмитрий");
+    timon.hello(timon);
+    log(db.GetPhrase('Run successful'));
+}
+
+if (module.parent) {
+    exports.Run = Run;
+} else {
+    Run();
+}
